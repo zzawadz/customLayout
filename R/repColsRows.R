4d8a0a5082fa2f2ce6396089ed8c74aa04f12175
zzawadz <zawadzkizygmunt@gmail.com>
2014-09-01 22:17:53 +0200

    New functions

---
+++ R/repColsRows.R
@@ -0,0 +1,21 @@
+
+layColRepGenerator = function(side)
+{
+  if(side == "widths") fnc = layColBind
+  if(side == "heights") fnc = layRowBind
+  
+  function(layout, n)
+  {
+    layall = layout
+    if(n > 1) for(i in 2:n)
+    {
+      size = c(i-1,1)
+      print(size)
+      layall = layColBind(layall,layout, size)
+    }
+    layall
+  }
+}
+
+layRepByCol = layColRepGenerator("widths")
+layRepByRow = layColRepGenerator("heights")
