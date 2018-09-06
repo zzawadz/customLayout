a434f3b2eb3e0d363f1b13b40061712a7f72bd21
Zygmunt Zawadzki <zygmunt@zstat.pl>
2018-09-06 12:51:08 +0200

    add config for cran and appveyor

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,4 +2,17 @@ library(testthat)
 library(customLayout)
 library(vdiffr)
 
+on_appveyor <- function() {
+  identical(Sys.getenv("APPVEYOR"), "True")
+}
+on_cran <- function() {
+  !identical(Sys.getenv("NOT_CRAN"), "true")
+}
+
+# Use minimal fonts.conf to speed up fc-cache
+if (on_appveyor() || on_cran()) {
+  gdtools::set_dummy_conf()
+}
+
+
 test_check("customLayout")
