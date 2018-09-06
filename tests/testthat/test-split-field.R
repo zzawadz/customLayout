52cf1c7416cc522d6a27eb34448c6cfcd9d0c15f
Zygmunt Zawadzki <zygmunt@zstat.pl>
2018-09-06 17:45:19 +0200

    test vardiff

--- tests/testthat/test-split-field.R
+++ tests/testthat/test-split-field.R
@@ -4,6 +4,7 @@ disp_basic_split_field <- function() {
   l1 <- layCreate(matrix(c(1:4), ncol = 2), widths = c(4, 1))
   l2 <- layCreate(matrix(c(1:4), ncol = 2), widths = c(1, 1))
   l3 <- laySplitField(l1, l2, 2)
+  stop("vardiff test")
   layShow(l3)
 }
 
