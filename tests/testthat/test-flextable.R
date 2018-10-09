86098b394b5312e3360a4f0b2c06b58fa1ab7fe4
Zygmunt Zawadzki <zygmunt@zstat.pl>
2018-10-09 14:41:43 +0200

    test font size in phl_calc_fontsize

--- tests/testthat/test-flextable.R
+++ tests/testthat/test-flextable.R
@@ -1,8 +1,9 @@
 context("Custom layout")
 
-test_that("Warn when the table is too wide", {
-  
-
-  
+test_that("Font size", {
+  x <- tail(iris, 10)[,c(1,5)]
+  expect_equivalent(
+    phl_calc_fontsize(x, 5),
+    c(24, 0.4545455),
+    tolerance = 0.0001)
 })
-
